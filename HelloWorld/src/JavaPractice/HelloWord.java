f8dfa834f0e6a7b72e4919c213191f2b455517c7
Chi10ya <mchintuk@yahoo.com>
2018-10-02 17:49:15 +0400

    adding hello world java program to git

---
+++ HelloWorld/src/JavaPractice/HelloWord.java
@@ -0,0 +1,18 @@
+package JavaPractice;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		int i=10;
+		
+		i=i+10;
+		
+		System.out.println(i);
+		System.out.println("Chaitanya Ambica Yash Dhiya");
+
+	}
+
+}
